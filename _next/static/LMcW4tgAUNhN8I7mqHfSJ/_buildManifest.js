a868ac44a2edb14fc56c837986a4d3eb59694670
yossuli <yossuli@users.noreply.github.com>
2023-10-02 15:39:20 +0000

    deploy: e644f3102aa8c9eef54ff890a634f6787717ada0

---
+++ _next/static/LMcW4tgAUNhN8I7mqHfSJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/abcb77bf46fe042c.css","static/chunks/pages/index-bf486999f36e0789.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
